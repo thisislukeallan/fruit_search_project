8c3b04fc168c829d7068266af409290684334bd6
Luke Allan <thisislukeallan@gmail.com>
2023-09-04 13:39:28 -0500

    added index.html, script.js, and style.css for Fruit Autocomplete Project

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+	// TODO
+	results = fruit.filter((fruitName) => fruitName.toLowerCase().includes(str.toLowerCase()));
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+	const inputVal = e.target.value;
+	showSuggestions(search(inputVal), inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = "";
+	if (inputVal !== "") {
+		results.forEach((fruitInResults) => {
+			let li = document.createElement("li");
+			li.innerText = fruitInResults;
+			suggestions.appendChild(li);
+		});
+	}	
+	// TODO
+}
+
+function useSuggestion(e) {
+	// TODO
+	let fruitChoice = e.target.innerText;
+	input.value = fruitChoice;
+	suggestions.innerHTML = "";
+}
+
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
